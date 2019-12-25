b173cfb4aeb6dd867d81696700a7b9e181aa895e
PowerPoint1 <57618710+PowerPoint1@users.noreply.github.com>
2019-12-25 09:41:23 +0300

    Add files via upload

--- 162.py
+++ 162.py
@@ -5,43 +5,59 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QMainWindow
 
 
-
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
-    
-a = {'Audi': 'Q7', 'Audi': 'TT', 'BMW': 'M5 E92', 'BMW': 'Nazca', 'Henessy': 'Venom GT', 'Ford': 'GT40', 'Ford': 'Focus', 'Porsche': 'Macan',
-'Porsche': '356A', 'Volvo': 'XC60', 'Volvo': '850R', 'Subaru': 'WRX STI', 'Subaru': 'Legacy RS', 'Alfa Romeo': 'TZ2',
-'Alfa Romeo': '4C', 'Noble': 'M600'}
-b = ['Q7', 'TT', 'Nazca', 'Focus', '356A', '850R', 'Legacy RS', 'M5 E92', 'Venom GT', 'GT40', 'Macan', 'XC60', 'WRX STI',
-'TZ2', '4C', 'M600']
+
+a = {'Audi': 'TT', 'BMW': 'Nazca', 'Henessy': 'Venom GT', 'Ford': 'GT40','Porsche': '356A',
+     'Volvo': '850R', 'Subaru': 'WRX STI', 'Alfa Romeo': 'TZ2', 'Noble': 'M600'}
+b = ['TT', 'Nazca', '356A', '850R', 'Venom GT', 'GT40', 'WRX STI', 'TZ2', 'M600']
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('aud.ui', self)
+        self.count = 0
         self.buttons = [self.pushButton_2, self.pushButton_3, self.pushButton_4, self.pushButton]
         self.titles = set()
-        self.pushButton_5.clicked.connect(self.run)
+        self.pushButton_5.clicked.connect(self.clear_and_start)
+        self.pushButton_2.clicked.connect(self.check)
+        self.pushButton_3.clicked.connect(self.check)
+        self.pushButton_4.clicked.connect(self.check)
+        self.pushButton.clicked.connect(self.check)
         
+    
+    def clear_and_start(self):
+        self.count = 0
+        self.run()
+
+
     def run(self):
+        self.titles.clear()
         self.flag = 0
         self.label.setText(random.choice(list(a.keys())))
+        self.label_2.setText(str(self.count))
         for i in self.buttons:
             number = random.randint(0, 100)
-            if (number in range(80, 100)) or (i == self.pushButton) and (not(self.flag)):
+            if ((number in range(80, 100)) or (i == self.pushButton)) and (not(self.flag)):
                 i.setText(a[self.label.text()])
+                self.titles.add(a[self.label.text()])
                 self.flag = 1
             else:
                 n = random.choice(b)
                 while n in self.titles or n == a[self.label.text()]:
                     n = random.choice(b)
                 i.setText(n)
-                self.titles.add(n)
+                self.titles.add(n)  
+                
+    def check(self):
+        if self.sender().text() == a[self.label.text()]:
+            self.count += 1
+            self.label_2.setText(str(self.count))
+        self.run()
             
-        
-        
-        
-        
+            
+            
+
 sys.excepthook = except_hook
 app = QApplication(sys.argv)
 ex = MyWidget()
